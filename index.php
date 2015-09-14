f6d14a49e263cdb7727da626c6a15182ef24a53a
easyguyme <easyguyme@users.noreply.github.com>
2015-09-14 17:20:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,10 @@
      <meta charset="UTF-8">
      <title>OOP in PHP</title>
 
-     <?php include("class.php");?>
+     <?php
+     //Include your class in your main PHP page
+     include("class.php");
+     ?>
 
 
 
@@ -16,12 +19,13 @@
 
  <body>
  <?php
+ //To create an object out of a class, you need to use the 'new' keyword.
  $stephan=new person();
  $jimmy =new person();
-
+// Set an objects properties
  $stephan->set_name("Stephan Mcgregory");
  $jimmy ->set_name("Jimmy Wanjala");
-
+ //Accessing an object's data using ->
  echo "Stephans full name is ".$stephan->get_name();
  echo "Jimmy's full name is".$jimmy->get_name();
  ?>
